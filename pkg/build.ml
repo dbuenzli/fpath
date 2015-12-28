a0dc0b8663349ab94d8f15c457b4ee81307c7154
Daniel Bünzli <daniel.buenzli@erratique.ch>
2015-12-28 10:29:09 +0000

    Fix toplevel support.

--- pkg/build.ml
+++ pkg/build.ml
@@ -7,5 +7,6 @@ let () =
     Pkg.lib "pkg/META";
     Pkg.lib ~exts:Exts.module_library "src/fpath";
     Pkg.lib ~exts:Exts.library "src/fpath_top";
+    Pkg.lib "src/fpath_top_init.ml";
     Pkg.doc "README.md";
     Pkg.doc "CHANGES.md"; ]
